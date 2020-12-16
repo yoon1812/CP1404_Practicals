3237805bd0b0d645afc5cfdc5908e28e29ad6d69
yoon <yoon1812>
2020-12-16 21:44:50 +0800

    Initial commit

---
+++ Prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+COLOR_CODE = {"aliceblue": "#f0f8ff", "thistle1": "#ffe1ff", "rosybrown1": "#ffc1c1", "red2": "#ee0000",
+              "pink": "#ffc0cb", "maroon": "#b03060", "mediumpurple": "#9370db", "palegreen": "#98fb98",
+              "palevioletred": "#db7093", "yellow1": "#ffff00"}
+
+color_name = input("Enter color name: ")
+if color_name in COLOR_CODE:
+    print(color_name, "is", COLOR_CODE[color_name])
+else:
+    print("Invalid color name")
+    state_code = input("Enter color name: ")
+
---
+++ Prac_05/word_occurrences.py
@@ -0,0 +1,8 @@
+text = input("Text: ")
+words = text.split()
+words.sort()
+word_count = {}
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+for key, value in word_count.items():
+    print("{} : {}".format(key,value))
